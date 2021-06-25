ccc51e1a5a718f735d2618b145271e1c0ac9fdf6
bezgaliba <74833724+bezgaliba@users.noreply.github.com>
2021-06-25 21:54:43 +0300

    C and M : Added Profile, Geocache, Comments

---
+++ Http/Controllers/ProfileController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index($id){
+        $id = User::findOrFail($id);
+        return view('home', [
+            'id' => $id,
+        ]);
+    }
+}
